042f0253479862be71a0d02949f3fb508dd71437
Rafal Wilinski <raf.wilinski@gmail.com>
2024-08-21 23:34:14 +0200

    update casks

--- paip.rb
+++ paip.rb
@@ -5,10 +5,10 @@ class Paip < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/RafalWilinski/paip/releases/download/v0.0.1/paip-macos-arm64"
-    sha256 "arm64_sha256_hash" # Replace with actual SHA256 hash
+    sha256 "d6ccf48f34d0b19367bc4e9a2444b35cae98ab6e24a39c4375c59c628de1be99" 
   else
     url "https://github.com/RafalWilinski/paip/releases/download/v0.0.1/paip-macos-x64"
-    sha256 "x64_sha256_hash" # Replace with actual SHA256 hash
+    sha256 "e1990cd9381d26e03eafb67954dca2f329a797c7b72ff153eb1ce48cb0bf7746"
   end
 
   def install
